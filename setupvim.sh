d75d7f5faffe3f36e842f79ed6165d509447cbc7
Huang <dphuang2@illinois.edu>
2016-10-06 19:55:18 -0500

    Updated setupvim.sh

--- setupvim.sh
+++ setupvim.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git clone https://github.com/gmarik/Vundle.vim.git ~/vimfiles/bundle/Vundle.vim
 git clone https://github.com/dphuang2/dotfiles.git ~/dotfiles
-copy %USERPROFILE%\dotfiles\vimrc-pc ~/_vimrc
+cp %USERPROFILE%\dotfiles\vimrc-pc ~/_vimrc
